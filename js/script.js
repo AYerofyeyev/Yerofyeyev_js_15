68f69f6bf09e14cae4ad64beb82121e072299d58
AYerofyeyev <a.yerofyeyev@gmail.com>
2017-04-24 17:45:33 +0300

    jQuery ajax request is functioning

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
+let serverAnswer = {};
+
 function ready() {
+
 
   let wrapper = document.querySelector(".wrapper");
   wrapper.style.padding = "50px";
@@ -11,20 +14,42 @@ function ready() {
   let query = document.querySelector(".query");
   query.style.width = "400px";
   query.style.lineHeight = "26px";
+  query.focus();
 
   let search = document.querySelector(".pure-button");
   search.addEventListener("click", pull);
 
+  let onload = document.querySelector(".onload");
+  onload.style.opacity = 0;
+
   let result = document.querySelector(".result");
-  result.style.margin = "0 2px";
+  result.style.margin = "60px 2px";
   result.style.border = "1px solid #c0c0c0";
 
+  pull();
+
   function pull() {
-    console.log(query.value);
-    let request = new XMLHttpRequest();
-    request.open("post", "/submit");
-    request.send(query.value);
-    console.log(request);
+    let defaultImages = result.querySelectorAll("img");
+    console.log(defaultImages);
+    if (defaultImages.length > 0) {
+      defaultImages.forEach(function(item, i, defaultImages){
+        item.remove();
+      });
+    }
+    $.getJSON("https://pixabay.com/api/?key=3118779-be29778b1b1db18e334fc6de3&q=" + query.value + "&image_type=photo",
+    function(serverAnswer){
+      console.log(serverAnswer);
+      let library = serverAnswer.hits;
+      console.log(library);
+      let image = [];
+      for (let i = 0; i < library.length; i++){
+        image[i] = document.createElement("img");
+        image[i].src = library[i].previewURL;
+        result.appendChild(image[i]);
+        console.log(image[i]);
+      }
+    });
+    return false;
   }
 }
 
